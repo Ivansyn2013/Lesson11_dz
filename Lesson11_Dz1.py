cb0e39a2236c4fcf60318cfbc5732915280cd5c0
ivan <ivansyn2013@yandex.ru>
2021-12-20 13:25:44 +0300

    первое задание

--- Lesson11_Dz1.py
+++ Lesson11_Dz1.py
@@ -1,1 +1,34 @@
 class Date:
+    def __init__(self, date_s):
+        self.date_s = str(date_s)
+        Date.valid_date(date_s)
+
+    @classmethod
+    def get_date(cls, date_s):
+        date_s = str(date_s)
+        date_list = []
+        date_list = list(map(int,(date_s.split('-'))))
+        return print(f' День {date_list[0]} Месяц {date_list[1]} Год {date_list[2]}')
+
+    @staticmethod
+    def valid_date(date_s):
+        date_s = str(date_s)
+        date_list = []
+        date_list = list(map(int, (date_s.split('-'))))
+        if len(date_list) != 3:
+            return print('Неправильно зажан форма даты')
+
+        if date_list[0] not in range(1,32) :
+            return print('Неправильно задан день')
+
+
+        if date_list[1] not in range(1,13) :
+            return print('Неправильно задан месяц')
+
+        if date_list[2] not in range(1900,2050) :
+            return print('Неправильно задан год')
+
+
+
+Date.get_date('22-10-2222')
+d1 = Date('22-102-22222')
